f84ebbf25ac6a28c2afec176d067b19f64a22a36
Sergi Martínez <sergi.martinez@gmail.com>
2017-05-15 19:54:01 +0200

    testing answer

--- src/main/java/com/blindbugs/swbot/application/controller/HookController.java
+++ src/main/java/com/blindbugs/swbot/application/controller/HookController.java
@@ -15,9 +15,14 @@ import static org.slf4j.event.Level.DEBUG;
 public class HookController {
     private static final Logger logger = LoggerFactory.getLogger("HookController");
 
-    @RequestMapping(method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
     public ResponseEntity<String> get(@RequestBody String entry) {
         logger.debug("RECEIVED: " + entry);
-        return new ResponseEntity<String>("OK", HttpStatus.OK);
+        String response="{\n" +
+                "  \"speech\": \"I don't know yet\",\n" +
+                "  \"displayText\": \"I don't know yet\",\n" +
+                "  \"source\": \"SWBot\"\n" +
+                "}";
+        return new ResponseEntity<String>(response, HttpStatus.OK);
     }
 }
