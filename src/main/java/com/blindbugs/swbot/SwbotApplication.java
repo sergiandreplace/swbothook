f98fd27e892b0a49e913f28b47911d3916edeb26
Sergi Martínez <sergi.martinez@gmail.com>
2017-05-15 18:31:19 +0200

    Initial commit

---
+++ src/main/java/com/blindbugs/swbot/SwbotApplication.java
@@ -0,0 +1,12 @@
+package com.blindbugs.swbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwbotApplication.class, args);
+	}
+}
