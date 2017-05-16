4a33cc28fa88d43fdd8483512219869ea4796639
Sergi Martínez <sergi.martinez@scmspain.com>
2017-05-16 12:39:40 +0200

    reduced set of characters

--- src/main/java/com/blindbugs/swbot/application/action/SubjectAction.java
+++ src/main/java/com/blindbugs/swbot/application/action/SubjectAction.java
@@ -22,13 +22,19 @@ public class SubjectAction {
         String conversationId = JsonPath.read(request, "$.result.contexts[0].parameters.telegram_chat_id");
         logger.info("conversationId=" + conversationId);
 
+        String action = JsonPath.read(request, "$.result.action");
+
         String response;
         if (characterName == null) {
             response = "Tell me the name of a character";
         } else {
             properCharacter = findCharacter(characterName);
             if (properCharacter != null) {
-                response = "What do you want to know about " + properCharacter + "?";
+                if ("ask.subject.height".equalsIgnoreCase(action)) {
+                    response = getHeightResponse(properCharacter);
+                } else {
+                    response = "What do you want to know about " + properCharacter + "?";
+                }
             } else {
                 response = "I don't know who " + characterName + " is";
             }
@@ -46,6 +52,29 @@ public class SubjectAction {
                 "    }\n" +
                 "  }\n" +
                 "}";
+    }
+
+    private String getHeightResponse(String properCharacter) {
+        String height;
+        switch (properCharacter) {
+            case "Luke Skywalker":
+                height = "1.70";
+                break;
+            case "Leia Organa":
+                height = "1.60";
+                break;
+            case "Darth Vader":
+                height = "2.10";
+                break;
+            case "Han Solo":
+                height = "1.85";
+                break;
+            default:
+                height = "0.5";
+                break;
+
+        }
+        return properCharacter + " is " + height + " meters tall";
     }
 
     private String findCharacter(String unknownCharacter) {
