85aea35b2a0c4b178bc3a824d485dc92cc1c2a3d
Sergi Martínez <sergi.martinez@gmail.com>
2017-05-16 22:40:46 +0200

    added caching to people repository

--- src/main/java/com/blindbugs/swbot/SwbotApplication.java
+++ src/main/java/com/blindbugs/swbot/SwbotApplication.java
@@ -2,10 +2,13 @@ package com.blindbugs.swbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = {"com.blindbugs.swbot.application"})
 @SpringBootApplication
+@EnableCaching
+
 public class SwbotApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/blindbugs/swbot/application/repository/PeopleRepository.java
+++ src/main/java/com/blindbugs/swbot/application/repository/PeopleRepository.java
@@ -6,10 +6,13 @@ import com.blindbugs.swbot.application.datasource.swapi.sw.StarWarsApi;
 import com.blindbugs.swbot.application.repository.mapper.PeopleMapper;
 import com.blindbugs.swbot.domain.people.People;
 import com.blindbugs.swbot.domain.people.PeopleCollection;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Repository;
 
 import javax.inject.Inject;
 import java.util.List;
 
+@Repository
 public class PeopleRepository implements PeopleCollection {
     private final StarWarsApi starWarsApi;
     private final PeopleMapper peopleMapper;
@@ -20,12 +23,14 @@ public class PeopleRepository implements PeopleCollection {
         this.peopleMapper = peopleMapper;
     }
 
+    @Cacheable(cacheNames = "people")
     @Override
     public People getPeople(int id) {
         ApiPeople apiPeople = starWarsApi.getPeople(id);
         return peopleMapper.map(apiPeople);
     }
 
+    @Cacheable(cacheNames = "people")
     @Override
     public List<People> findPeople(String name) {
         ApiSWModelList<ApiPeople> peoplePage = starWarsApi.findPeople(name);
