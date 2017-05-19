f0627dbecfdc6456b31bd7e4516805289c37159e
Sergi Martínez <sergi.martinez@gmail.com>
2017-05-19 19:45:22 +0200

    Improving signal endpoint to be Hoff-approved

--- src/main/java/com/blindbugs/swbot/application/controller/SignalController.java
+++ src/main/java/com/blindbugs/swbot/application/controller/SignalController.java
@@ -1,6 +1,7 @@
 package com.blindbugs.swbot.application.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,8 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/check")
 public class SignalController {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
     public ResponseEntity<String> get() {
-        return new ResponseEntity<String>("OK", HttpStatus.OK);
+        return new ResponseEntity<String>("<html>\n" +
+                "<body>\n" +
+                "<img src=\"http://place-hoff.com/800/600\" />\n" +
+                "<h1>The Hoff approves this!</h1>\n" +
+                "</body>\n" +
+                "</html>", HttpStatus.OK);
     }
 }
